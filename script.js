7d35b17bc8c1c618b8e4437246f8c157afe42ac1
Angelium Eddra <angelium.eddra1972@gmail.com>
2023-09-17 16:34:41 -0300

    versão front-end funcional

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-function save_nums(){
+var valor_final = 0.00;
+var valor = 5.00;
+
+function inserirNumero(selecionado){
+    var mostrar_valor_final = document.getElementById('valor');
+    valor_final += valor;
+
+    mostrar_valor_final.innerHTML = "Preço final: R$" + valor_final;
+    // inserir no formulário o numero selecionado
     
+}
+
+function removerNumero(selecionado){
+    var mostrar_valor_final = document.getElementById('valor');
+    valor_final -= valor;
+    
+    mostrar_valor_final.innerHTML = "Preço final: R$" + valor_final;
+    // remover do formulário o numero selecionado
+}
+
+
+
+function escolhido(numero_clicado){
+    var marcado = document.getElementById(numero_clicado);
+    var estado = getComputedStyle(marcado).backgroundColor;
+    
+    
+    
+    if(estado == "rgb(0, 0, 255)"){
+        marcado.style.backgroundColor = "rgb(128, 128, 128)"
+        inserirNumero(numero_clicado);
+    }
+    else if(estado == "rgb(128, 128, 128)"){
+        marcado.style.backgroundColor = "rgb(0, 0, 255)"
+        removerNumero(numero_clicado);
+    }
 }
